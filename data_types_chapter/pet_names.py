cdeeec3e85681723ef3cf111549e48d424f02804
Cody Reimers <ceriousackount@gmail.com>
2024-02-25 04:15:48 +0000

    Added another chapter

---
+++ data_types_chapter/pet_names.py
@@ -0,0 +1,8 @@
+names = (
+    "Asta",
+    "Butterscotch",
+    "Pudding",
+    "Neptune",
+    "Darwin",
+)
+print("This is a tuple!\n" + str(names))
---
+++ data_types_chapter/pet_types.py
@@ -0,0 +1,9 @@
+pets = {
+    "Asta": "dog",
+    "Butterscotch": "cat",
+    "Pudding": "cat",
+    "Neptune": "fish",
+    "Darwin": "lizard",
+}
+print("These are my pets and what kinds of animals they are!")
+print(pets)
