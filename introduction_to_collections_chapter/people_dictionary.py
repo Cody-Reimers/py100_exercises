4936ce6774447f9355eae871cbd7fd5b0351f815
Cody Reimers <ceriousackount@gmail.com>
2024-02-25 04:21:26 +0000

    Added another chapter

---
+++ introduction_to_collections_chapter/people_dictionary.py
@@ -0,0 +1,11 @@
+people = {
+    "Alice": "USA",
+    "Francois": "Canada",
+    "Inti": "Peru",
+    "Monika": "Germany",
+    "Sanyu": "Uganda",
+    "Yoshitaka": "Japan",
+}
+print("Whose country do you want to know?")
+name = input()
+print(people[name])
---
+++ introduction_to_collections_chapter/pi.py
@@ -0,0 +1,3 @@
+pi = 3.141592
+str_pi = str(pi)
+print(f"{pi} and {str_pi}")
