ea8cf0fe5a33c923c4ae80b2ed2f4f404c57a302
Cody Reimers <ceriousackount@gmail.com>
2024-02-25 04:24:59 +0000

    Added another chapter

---
+++ more_stuff_chapter/square_root.py
@@ -0,0 +1,8 @@
+import math
+from math import sqrt
+import math as m
+
+number = int(input("What number do you want the square root of? "))
+print(math.sqrt(number))
+print(sqrt(number))
+print(m.sqrt(number))
