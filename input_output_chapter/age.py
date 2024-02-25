d8c2fcaf0edc1a1c8a0ed688407cc923968b32ad
Cody Reimers <ceriousackount@gmail.com>
2024-02-25 04:18:36 +0000

    Added another chapter

---
+++ input_output_chapter/age.py
@@ -0,0 +1,7 @@
+age = int(input("How old are you? "))
+
+print(f"\nYou are {age} years old.")
+print(f"In 10 years you will be {age + 10} years old.")
+print(f"in 20 years you will be {age + 20} years old.")
+print(f"In 30 years you will be {age + 30} years old.")
+print(f"In 40 years you will be {age + 40} years old.")
